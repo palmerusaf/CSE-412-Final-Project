83a2d151aefa2b307f53fe1ce47a0efe2950c623
Branden Palmer <brandenpalmer08@gmail.com>
2025-04-21 21:34:02 +0700

    add boilerplate for projecrts

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,24 @@ function App() {
     <SideBar
       menuData={[
         {
-          menu: "main menu",
-          icon: icon.AArrowDown,
+          menu: "Projects",
+          icon: icon.LucideTelescope,
           items: [
             {
-              subMenu: "subMenu",
-              content: "foo",
+              subMenu: "Create",
+              content: <NotImplemented />,
+            },
+            {
+              subMenu: "Select",
+              content: <NotImplemented />,
+            },
+            {
+              subMenu: "Update",
+              content: <NotImplemented />,
+            },
+            {
+              subMenu: "Delete",
+              content: <NotImplemented />,
             },
           ],
         },
@@ -20,3 +32,9 @@ function App() {
 }
 
 export default App;
+
+const NotImplemented = () => (
+  <div className="flex flex-col justify-center content-center w-full h-full text-center">
+    Not Implemented.
+  </div>
+);
