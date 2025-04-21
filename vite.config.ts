f550c4ac404d96035936c4778c15786e9f6cdf30
Branden Palmer <brandenpalmer08@gmail.com>
2025-04-21 22:39:21 +0700

    add cd/ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "CSE-412-Final-Project",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
