dcf5e731121625088ce8a5a75841c5d0bb42b7ab
Branden Palmer <brandenpalmer08@gmail.com>
2025-04-22 16:16:45 +0700

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,59 @@
 import { SideBar } from "@/components/sidebar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as icon from "lucide-react";
+const queryClient = new QueryClient();
 function App() {
   return (
-    <SideBar
-      menuData={[
-        {
-          menu: "Projects",
-          icon: icon.LucideTelescope,
-          items: [
-            {
-              subMenu: "Create",
-              content: <NotImplemented />,
-            },
-            {
-              subMenu: "Select",
-              content: <NotImplemented />,
-            },
-            {
-              subMenu: "Update",
-              content: <NotImplemented />,
-            },
-            {
-              subMenu: "Delete",
-              content: <NotImplemented />,
-            },
-          ],
-        },
-        {
-          menu: "Todos",
-          icon: icon.ListTodoIcon,
-          items: [
-            {
-              subMenu: "Create",
-              content: <NotImplemented />,
-            },
-            {
-              subMenu: "Select",
-              content: <NotImplemented />,
-            },
-            {
-              subMenu: "Update",
-              content: <NotImplemented />,
-            },
-            {
-              subMenu: "Delete",
-              content: <NotImplemented />,
-            },
-          ],
-        },
-      ]}
-    />
+    <QueryClientProvider client={queryClient}>
+      <SideBar
+        menuData={[
+          {
+            menu: "Projects",
+            icon: icon.LucideTelescope,
+            items: [
+              {
+                subMenu: "Create",
+                content: <NotImplemented />,
+              },
+              {
+                subMenu: "Select",
+                content: <NotImplemented />,
+              },
+              {
+                subMenu: "Update",
+                content: <NotImplemented />,
+              },
+              {
+                subMenu: "Delete",
+                content: <NotImplemented />,
+              },
+            ],
+          },
+          {
+            menu: "Todos",
+            icon: icon.ListTodoIcon,
+            items: [
+              {
+                subMenu: "Create",
+                content: <NotImplemented />,
+              },
+              {
+                subMenu: "Select",
+                content: <NotImplemented />,
+              },
+              {
+                subMenu: "Update",
+                content: <NotImplemented />,
+              },
+              {
+                subMenu: "Delete",
+                content: <NotImplemented />,
+              },
+            ],
+          },
+        ]}
+      />
+    </QueryClientProvider>
   );
 }
 
