cb5272e526d048a1e855946af26b02de700095bf
Branden Palmer <brandenpalmer08@gmail.com>
2025-04-22 15:52:25 +0700

    add gendb script

--- src/backend/createTbleSqlRaw.ts
+++ src/backend/createTbleSqlRaw.ts
@@ -1,6 +1,18 @@
 export const createTbleSqlRaw = `
-CREATE TABLE "users" (
-	"id" integer,
-	"first_name" varchar NOT NULL
+CREATE TABLE "projects" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "projects_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" text NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL
 );
+--> statement-breakpoint
+CREATE TABLE "todos" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "todos_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" text NOT NULL,
+	"description" text,
+	"completed" boolean DEFAULT false NOT NULL,
+	"projectId" integer NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "todos" ADD CONSTRAINT "todos_projectId_projects_id_fk" FOREIGN KEY ("projectId") REFERENCES "public"."projects"("id") ON DELETE no action ON UPDATE no action;
 `;
--- src/backend/db.ts
+++ src/backend/db.ts
@@ -1,15 +1,11 @@
 import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
-import { users } from "./schema";
 import { createTbleSqlRaw } from "./createTbleSqlRaw";
 
 const client = new PGlite("idb://my-pgdata");
+
+if (import.meta.env.DEV) indexedDB.deleteDatabase("my-pgdata");
+
 client.exec(createTbleSqlRaw);
 await client.waitReady;
 const db = drizzle({ client });
-export async function test(input: string) {
-  await db.insert(users).values({ first_name: input });
-  const res = await db.select().from(users);
-  await db.delete(users);
-  return res;
-}
