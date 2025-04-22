7ec0703fad84db8e00cda11f3f594edf3ff8812b
Branden Palmer <brandenpalmer08@gmail.com>
2025-04-22 14:22:36 +0700

    update schema

--- src/backend/schema.ts
+++ src/backend/schema.ts
@@ -1,6 +1,19 @@
-import { pgTable, integer, varchar } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: integer(),
-  first_name: varchar().notNull(),
+export const projects = t.pgTable("projects", {
+  id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: t.text().notNull(),
+  createdAt: t.timestamp().defaultNow().notNull(),
+});
+
+export const todos = t.pgTable("todos", {
+  id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: t.text().notNull(),
+  description: t.text(),
+  completed: t.boolean().default(false).notNull(),
+  projectId: t
+    .integer()
+    .references(() => projects.id)
+    .notNull(),
+  createdAt: t.timestamp().defaultNow().notNull(),
 });
